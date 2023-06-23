f3939567b03e2a769c55c537cdc42c5e55a161a3
Nasif Shahriar Mohim <53572742+shahriarnasif@users.noreply.github.com>
2023-06-24 00:14:06 +0600

    sentiment-analysis

--- app.py
+++ app.py
@@ -1,51 +1,41 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import torch
 
-# Initialize the Flask application
+model_name = "StatsGary/setfit-ft-sentinent-eval"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
 app = Flask(__name__)
 
-# Set the database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postges:abed11@10.0.0.31/mydatabase'
+@app.route("/analyze", methods=["POST"])
+def analyze_sentiment():
+    try:
+        
+        text = request.json.get("text")
 
-# Create the SQLAlchemy database instance
-db = SQLAlchemy(app)
+        
+        if text is None:
+            error_message = "Missing 'text' parameter in the request"
+            return jsonify({"error": error_message}), 400
 
-# Define the model for the database table
-class Example(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String())
-    password = db.Column(db.String())
+        
+        inputs = tokenizer(text, return_tensors="pt")
 
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
+        
+        outputs = model(**inputs)
+        predicted_class = torch.argmax(outputs.logits).item()
+        sentiment = ["negative", "neutral", "positive"][predicted_class]
 
-    def __repr__(self):
-        return '<Example %r>' % self.email
+        
+        response = {"sentiment": sentiment}
 
-# Define a route to return a JSON response when a GET request is made
-@app.route('/api/get', methods=['GET'])
-def example_api():
-    examples = Example.query.all()
-    response = []
-    for example in examples:
-        response.append({
-            'id': example.id,
-            'email': example.email,
-            'password': example.password
-        })
-    return jsonify(response)
+        return jsonify(response)
 
-# Define a route to handle a POST request
-@app.route('/api/post', methods=['POST'])
-def add_example():
-    email = request.json['email']
-    password = request.json['password']
-    example = Example(email, password)
-    db.session.add(example)
-    db.session.commit()
-    return jsonify({'message': 'Example added successfully!'})
-
-# Run the application
-if __name__ == '__main__':
-    app.run(debug=True)
+    except Exception as e:
+        
+        error_message = "An error occurred while analyzing the sentiment"
+        return jsonify({"error": error_message}), 500
+if __name__ == "__main__":
+    app.run()
